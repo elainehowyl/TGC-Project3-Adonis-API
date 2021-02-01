f661872bcec2d38b620df929ddc354b038addaa8
elainehowyl <elaine_0110@hotmail.com>
2021-02-01 05:23:47 +0000

    able to process new creation of food

--- app/Controllers/Http/FoodController.js
+++ app/Controllers/Http/FoodController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Foods = use('App/Models/Food')
+const Category = use('App/Models/Category')
 
 class FoodController {
   async index({response}){
@@ -13,9 +14,23 @@ class FoodController {
       'foods':foods.toJSON()
     })
   }
-  async create({}){
+  async create({view}){
+    let category = await Category.all()
+    return view.render('food/createfood', {
+      'category':category.toJSON()
+    })
+  }
+  async processCreate({}){
     let body = request.post()
     let newFood = new Foods()
+    newFood.name = body.name
+    newFood.description = body.description
+    newFood.price = body.price
+    newFood.image_source = body.image_source
+    await newFood.save()
+    await newFood.categories().attach(body.category)
+    // response.json(newFood)
+    response.route('foodList')
   }
 }
 
