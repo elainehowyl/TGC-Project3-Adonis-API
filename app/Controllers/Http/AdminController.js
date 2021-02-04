95b549397699cc8bbdf2379c8b1d67e682f162bd
elainehowyl <elaine_0110@hotmail.com>
2021-02-04 09:04:01 +0000

    finished validation for user registration

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { validateAll } = use('Validator')
 const Admin = use('App/Models/Admin')
 const Hash = use('Hash')
 
@@ -7,8 +8,26 @@ class AdminController {
   register({view}){
     return view.render('adminregister')
   }
-  async processRegister({request,response}){
+  async processRegister({request,response,session}){
+    const rules = {
+      username:'required|unique:admins',
+      password:'required|confirmed|min:8',
+    }
+    const messages = {
+     'username.required':'Please provide a username',
+     'username.unique':'Username already exist',
+     'password.required':'Please provide a password',
+     'password.min':'Password should be at least 8 characters long',
+     'password.confirmed':'Passwords do not match'
+    }
     let body = request.post()
+    const validation = await validateAll(body, rules, messages)
+    if (validation.fails()) {
+      session
+        .withErrors(validation.messages())
+        .flashAll()
+      return response.redirect('back')
+    }
     let newAdmin = new Admin()
     newAdmin.username = body.username
     newAdmin.password = body.password
