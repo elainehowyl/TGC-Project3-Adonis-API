1d53eb3f47fd50c1d0d9d3897b40149e90426545
elainehowyl <elaine_0110@hotmail.com>
2021-04-17 18:00:47 +0000

    added stripe codes but havent tested it out yet

--- app/Controllers/Http/CheckoutController.js
+++ app/Controllers/Http/CheckoutController.js
@@ -1,40 +1,56 @@
 'use strict'
 
-// import config
 const Config = use('Config')
-// import stripe.js from config folder and get secret key
 const Stripe = use('stripe')(Config.get('stripe.secret_key'))
 
-// idk what does this refer to?
+// what is this for ??
+// i probably don't need it
 const CART_KEY = "cart"
 
 class CheckoutController {
-  async checkout({ response, session, view }){
+  async checkout({response, session, view}) {
+    // 1. create line items
+    // retrieve line items with cart ID?
     let lineItems = []
-    let cart = Object.values(session.get(CART_KEY, {}))
-    // Object.values() method takes an object as argument, returns an array that contains the values of the object
-    // 1. Create line items
-    for(let cartItem of cart){
-      lineItems.push({
-        name: cartItem.title,
-        images: [cartItem.image_url],
-        amount: cartItem.price,
-        quantity: cartItem.qty,
-        currency:'SGD'
-      })
-    }
     let metaData = JSON.stringify(Object.values(cart))
-    // 2. Create payment
+    // 2. create payment
     const payment = {
-      payment_method_types: ['card'],
+      payment_method_types = ['card'],
       line_items: lineItems,
       success_url: Config.get('stripe.success_url') + '?sessionId={CHECKOUT_SESSION_ID}',
       cancel_url: Config.get('stripe.error_url'),
-      metadata:{
+      metadata: {
         'orders': metaData
       }
     }
+    // 3. register payment
+    let stripeSession = await Stripe.checkout.sessions.create(payment)
+    return view.render('checkout/checkout', {
+      'sessionId':stripeSession.id, // Id of the session
+      'publishableKey':Config.get('stripe.publishable_key')
+    })
+  }
+
+  processPayment({request, response}) {
+    let payload = request.raw()
+    let endpointSecret = Config.get('stripe.endpoint_secret')
+    let sigHeader = request.header("stripe-signature")
+    let event = null
+    try {
+      event = Stripe.webhooks.constructEvent(payload, sigHeader, endpointSecret)
+    } catch(error) {
+      response.send({
+        'error':e.message
+      })
+    }
+    if (event.type == 'checkout.session.completed') {
+      let stripeSession = event.data.object
+    }
+    //process stripe session
+    response.json({received:true})
   }
 }
 
 module.exports = CheckoutController
+
+
